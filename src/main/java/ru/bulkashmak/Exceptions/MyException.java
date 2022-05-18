8181258292464d5e00e98fb92f9416c0e5e66673
Bulat Maskurov <mbmhd2015@gmail.com>
2022-05-18 19:50:36 +0300

    20. Постоянно отлавливать исключения и просто выполнять программу дальше - не очень хороший подход. Зачастую таким образом просто откладываешь падение программы до следующего NullPointerException, и тогда программа упадёт уже из-за следствия, а не причины.

---
+++ src/main/java/ru/bulkashmak/Exceptions/MyException.java
@@ -0,0 +1,10 @@
+package ru.bulkashmak.Exceptions;
+
+public class MyException extends Exception {
+
+    public MyException() {}
+
+    public MyException(String message) {
+        super(message);
+    }
+}
